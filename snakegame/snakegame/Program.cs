d27fb336da0baf143b886e86244a34b41be0b827
st.jimmy <st.jimmy-88@mail.ru>
2017-03-23 22:45:21 +0200

    Использование класса Список

--- snakegame/snakegame/Program.cs
+++ snakegame/snakegame/Program.cs
@@ -16,6 +16,25 @@ namespace snakegame
             Point p2 = new Point(4,5,'#');
             p2.DrawSymbol();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.Remove(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
             Console.ReadLine();
         }
     }
