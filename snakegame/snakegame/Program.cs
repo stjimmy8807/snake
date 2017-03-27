025968f3dccf456a3ce1c4c4791be8e428b8b8bf
st.jimmy <st.jimmy-88@mail.ru>
2017-03-27 07:06:34 +0300

    Прорисовка вертикальной линии и фигуры

--- snakegame/snakegame/Program.cs
+++ snakegame/snakegame/Program.cs
@@ -10,17 +10,14 @@ namespace snakegame
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.DrawSymbol();
-
-            Point p2 = new Point(4,5,'#');
-            p2.DrawSymbol();
-
-            HorizontalLine lineH = new HorizontalLine(5,10,8,'+');
-            lineH.Drow();
-
-            VerticalLine lineV = new VerticalLine(12,3,10,'&');
-            lineV.Drow();
+            HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0,24,0,'+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
             Console.ReadLine();
         }
