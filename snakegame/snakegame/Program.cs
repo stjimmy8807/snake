df0ffa1ed9ca579477a55592974da7a90e23d7fd
st.jimmy <st.jimmy-88@mail.ru>
2017-03-27 21:54:45 +0300

    Прорисовка змейки

--- snakegame/snakegame/Program.cs
+++ snakegame/snakegame/Program.cs
@@ -21,6 +21,10 @@ namespace snakegame
             leftLine.Drow();
             rightLine.Drow();
 
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
             Console.ReadLine();
         }
     }
